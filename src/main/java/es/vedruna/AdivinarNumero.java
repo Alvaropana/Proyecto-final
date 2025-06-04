d95d89879f8cb30c163455ae20c1756e876a9b26
Alvaropana <alvaro.pena@a.vedrunasevillasl.es>
2025-06-04 20:01:25 +0200

    Estructura de los metodoso

---
+++ src/main/java/es/vedruna/AdivinarNumero.java
@@ -0,0 +1,31 @@
+package es.vedruna;
+import java.util.Scanner;
+
+public class AdivinarNumero {
+    //empezar el juego
+    public static void EmpezaraJugar(){
+
+    }
+
+    // general el numero aleatorio
+    public static  int  generealNumeroAleatorio(){
+        return 12;
+    }
+
+    //Introducir el numero del jugador
+    public static int introducirNumero(){
+        return 12;
+    }
+
+    //Mostra las pista al jugador
+    public static void mostrarPista(){
+
+    }
+
+    //Mostrar el ganador
+    public static void mostrarGanador(){
+
+    }
+
+
+}
--- src/main/java/es/vedruna/Main.java
+++ src/main/java/es/vedruna/Main.java
@@ -2,6 +2,8 @@ package es.vedruna;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    AdivinarNumero.EmpezaraJugar();
+
     }
 }
