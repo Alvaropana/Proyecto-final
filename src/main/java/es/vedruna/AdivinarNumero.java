34e0e40d60298aa02e73bb350ddb1c43800bd63a
Alvaropana <alvaro.pena@a.vedrunasevillasl.es>
2025-06-05 11:46:23 +0200

    Metodos funcionales y modificacion del metodo introducirNumero para que sea valido

--- src/main/java/es/vedruna/AdivinarNumero.java
+++ src/main/java/es/vedruna/AdivinarNumero.java
@@ -1,31 +1,61 @@
 package es.vedruna;
+import java.util.Random;
 import java.util.Scanner;
 
 public class AdivinarNumero {
-    //empezar el juego
-    public static void EmpezaraJugar(){
+    //Metodo para jugar 
+    public static void jugar() {
+        Scanner scanner = new Scanner(System.in);
+        int numeroObjetivo = generarNumeroAleatorio(0, 100);
+        int rondas = 0;
+        int numeroJugador;
 
+        System.out.println("Adivina el número entre 0 y 100.");
+        do {
+            numeroJugador = IntroducirNumeroValido(scanner, 0, 100);
+            rondas++;
+            mostrarPista(numeroJugador, numeroObjetivo);
+        } while (numeroJugador != numeroObjetivo);
+
+        mostrarGanador(rondas);
+    }
+    // metodo que genera el numero que hay que adivinar
+    public static int generarNumeroAleatorio(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min;
     }
 
-    // general el numero aleatorio
-    public static  int  generealNumeroAleatorio(){
-        return 12;
+    // Este metodo lo que hace es pedir el numeor al jugador y tambien valida que el jugador introduzca un numero dentro del rango y que solo queta nuemeros
+    public static int IntroducirNumeroValido(Scanner scanner, int min, int max) {
+        int numero;
+
+        while (true) {
+            System.out.print("Introduce un número entre: ");
+            try {
+                numero = scanner.nextInt();
+                if (numero >= min && numero <= max) {
+                    return numero;
+                } else {
+                    System.out.println("Número fuera de rango.");
+                }
+            } catch (Exception e) {
+                System.out.println("Entrada inválida. Solo se permiten números enteros.");
+                scanner.next();
+            }
+        }
     }
-
-    //Introducir el numero del jugador
-    public static int introducirNumero(){
-        return 12;
+    // Muetra si el jugador a in troducido un numero menor y mayor al que tiene que adivinar
+    public static void mostrarPista(int numeroUsuario, int numeroObjetivo) {
+        if (numeroUsuario < numeroObjetivo) {
+            System.out.println("Es mayor.");
+        } else if (numeroUsuario > numeroObjetivo) {
+            System.out.println("Es menor.");
+        }
     }
-
-    //Mostra las pista al jugador
-    public static void mostrarPista(){
-
+    // Muestra que has gando y te dice cunatos intentos has tadardo qeu adivinarlo
+    public static void mostrarGanador(int rondas) {
+        System.out.println("¡Felicidades! Has adivinado el número.");
+        System.out.println("Intentos: " + rondas);
     }
-
-    //Mostrar el ganador
-    public static void mostrarGanador(){
-
-    }
-
 
 }
--- src/main/java/es/vedruna/Main.java
+++ src/main/java/es/vedruna/Main.java
@@ -3,7 +3,7 @@ package es.vedruna;
 public class Main {
     public static void main(String[] args) {
 
-    AdivinarNumero.EmpezaraJugar();
+    AdivinarNumero.jugar();
 
     }
 }
